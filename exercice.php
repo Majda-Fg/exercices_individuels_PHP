bf5ae1fb1df1bc638237d7c1196f84751b6ea884
Majda-Fg <majda.fougou@gmail.com>
2025-02-08 01:07:48 +0100

    étape 2

--- exercice.php
+++ exercice.php
@@ -1,7 +1,13 @@
+<form action="exercice.php" method="POST">
+ <p>Votre nom : <input type="text" name="first_name" /></p>
+ <p><input type="submit" value="OK"></p>
+</form>
 <?php
-if($_GET["first_name"] == null){
-    echo 'Bonjour anonyme';
-}else{echo 'Bonjour ' . htmlspecialchars($_GET["first_name"]);}
+if(!empty($_POST['first_name'])){
+    echo 'Bonjour ' . htmlspecialchars($_POST["first_name"]);
+}elseif(!empty($_GET['first_name'])){
+    echo 'Bonjour ' . htmlspecialchars($_GET["first_name"]);
+}else{echo 'Bonjour anonyme';}
 
 // http://localhost/exercice_individuel_PHP/exercice.php?first_name=Majda
 // résultat : Bonjour Majda
